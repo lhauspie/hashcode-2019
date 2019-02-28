d7d86e731017a56a614860aea96474e5c3321cc0
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-02-28 20:08:10 +0100

    Added main score computation

--- Riduidel/src/main/java/com/zenika/hashcode/hashcode2019/output/Slideshow.java
+++ Riduidel/src/main/java/com/zenika/hashcode/hashcode2019/output/Slideshow.java
@@ -2,9 +2,11 @@ package com.zenika.hashcode.hashcode2019.output;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class Slideshow {
 	public Collection<Slide> slides = new ArrayList<Slide>();
+	private long score = -1;
 
 	public String toFile() {
 		StringBuilder sOut = new StringBuilder();
@@ -13,5 +15,26 @@ public class Slideshow {
 			sOut.append(s.toFile()).append("\n");
 		}
 		return sOut.toString();
+	}
+
+	public long score() {
+		if(score<0) {
+			score = computeScore();
+		}
+		return score;
+	}
+
+	private long computeScore() {
+		long score = 0;
+		Slide previous = null;
+		Iterator<Slide> iter = slides.iterator();
+		while(iter.hasNext()) {
+			Slide s = iter.next();
+			if(previous!=null) {
+				score = previous.score(s);
+			}
+			previous = s;
+		}
+		return score;
 	}
 }
