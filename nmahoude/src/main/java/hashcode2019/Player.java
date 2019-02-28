3c301db4a313cad53d6e9e06c16e1e6db3781114
nmahoude <nmahoude@gmail.com>
2019-02-28 18:43:13 +0100

    yolo

--- nmahoude/src/main/java/hashcode2019/Player.java
+++ nmahoude/src/main/java/hashcode2019/Player.java
@@ -1,5 +1,20 @@
 package hashcode2019;
 
-public class Player {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Player {
+  static final String RESOURCES_FOLDER = "d:\\tmp\\hashcode2019\\";
+  
+  public static void main(String[] args) {
+    String basename = "c_no_hurry"; // 16_750_973
+    File inputFile = new File(RESOURCES_FOLDER + basename + ".in");
+
+    try (Scanner sc = new Scanner(inputFile)) {
+
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
 }
