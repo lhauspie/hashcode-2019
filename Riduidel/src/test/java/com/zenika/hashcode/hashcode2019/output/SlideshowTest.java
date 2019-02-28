d9013754f89ccabec1b7ff2ab86fbd6f07211935
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-02-28 20:50:17 +0100

    Added a cool score test

---
+++ Riduidel/src/test/java/com/zenika/hashcode/hashcode2019/output/SlideshowTest.java
@@ -0,0 +1,45 @@
+package com.zenika.hashcode.hashcode2019.output;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.zenika.hashcode.hashcode2019.input.Picture;
+
+public class SlideshowTest  {
+
+	@Test
+	public void test_score_for_0_1_2_3() {
+		Slideshow s = new Slideshow();
+		s.slides.addAll(
+				Arrays.asList(
+/* 0 */				new HorizontalSlide(new Picture().tagged(Arrays.asList("cat", "beach", "sun"))),
+				new VerticalSlide(
+/* 1 */						new Picture().tagged(Arrays.asList("selfie", "smile")),
+/* 2 */						new Picture().tagged(Arrays.asList("garden", "selfie"))),
+/* 3 */				new HorizontalSlide(new Picture().tagged(Arrays.asList("garden", "cat")))
+				));
+		Assert.assertThat(s.score(), Is.is(1l));
+
+	}
+
+	@Test
+	public void test_score_for_0_3_1_2() {
+		Slideshow s = new Slideshow();
+		s.slides.addAll(
+				Arrays.asList(
+/* 0 */				new HorizontalSlide(new Picture().tagged(Arrays.asList("cat", "beach", "sun"))),
+/* 3 */				new HorizontalSlide(new Picture().tagged(Arrays.asList("garden", "cat"))),
+				new VerticalSlide(
+/* 1 */						new Picture().tagged(Arrays.asList("selfie", "smile")),
+/* 2 */						new Picture().tagged(Arrays.asList("garden", "selfie")))
+				));
+		Assert.assertThat(s.score(), Is.is(2l));
+
+	}
+
+}
