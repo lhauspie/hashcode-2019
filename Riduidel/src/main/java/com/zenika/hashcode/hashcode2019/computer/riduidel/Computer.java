c807e6c976f8a75f2c326fa6aa15519840c07762
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-02-28 20:33:53 +0100

    Implemented a semi-working computer (which will fail on big numbers)

--- Riduidel/src/main/java/com/zenika/hashcode/hashcode2019/computer/riduidel/Computer.java
+++ Riduidel/src/main/java/com/zenika/hashcode/hashcode2019/computer/riduidel/Computer.java
@@ -1,29 +1,57 @@
 package com.zenika.hashcode.hashcode2019.computer.riduidel;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 import com.zenika.hashcode.hashcode2019.input.Input;
 import com.zenika.hashcode.hashcode2019.input.Picture;
+import com.zenika.hashcode.hashcode2019.output.HorizontalSlide;
 import com.zenika.hashcode.hashcode2019.output.Slideshow;
 
 public class Computer {
-	private static class ByScoreComparator implements Comparator<Slideshow>{
+	public static class ByScoreComparator implements Comparator<Slideshow> {
 
 		@Override
 		public int compare(Slideshow o1, Slideshow o2) {
-			o1.score();
-			return 0;
+			return (int) (o2.score()-o1.score());
 		}
 		
 	}
-
 	public Slideshow compute(Input inputStructure) {
-		Collection<Slideshow> solutions = new TreeSet<Slideshow>(new ByScoreComparator());
-		for (Picture p : inputStructure.pictures) {
-			
+		return compute(inputStructure.pictures);
+	}
+
+	private Slideshow compute(List<Picture> pictures) {
+		Collection<Slideshow> slideshows = generate(new Slideshow(), pictures);
+		Set<Slideshow> sorted = new TreeSet<>(new ByScoreComparator());
+		sorted.addAll(slideshows);
+		return sorted.iterator().next();
+	}
+
+	private Collection<Slideshow> generate(Slideshow parent, List<Picture> pictures) {
+		Collection<Slideshow> returned = new ArrayList<>();
+		if(pictures.isEmpty()) {
+			returned.add(parent);
+		} else {
+			for(Picture p : pictures) {
+				List<Picture> others = new ArrayList<>();
+				others.addAll(pictures);
+				others.remove(p);
+				returned.addAll(generate(createSlideshowWith(parent, p), others));
+			}
 		}
+		return returned;
+	}
+
+	private Slideshow createSlideshowWith(Slideshow s, Picture p) {
+		Slideshow returned = new Slideshow();
+		returned.slides.addAll(s.slides);
+		returned.slides.add(new HorizontalSlide(p));
+		return returned;
 	}
 
 }
